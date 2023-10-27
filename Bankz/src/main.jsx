ad540b550f3e6e6f6277b2fc26cb878c4975c163
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-10-27 15:15:56 +0000

    redux

--- Bankz/src/main.jsx
+++ Bankz/src/main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 
 import store from "./store.jsx";
 
-store.dispatch({ type: "account/deposit", payload: 200 });
+// store.dispatch({ type: "account/deposit", payload: 200 });
 
-console.log(store.getState());
+// console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
